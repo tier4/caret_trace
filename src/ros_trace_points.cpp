6e23c8fe6e0aacd99bbb5ea86efc093d0c107f94
hsgwa <hasegawa.ats@gmail.com>
2021-10-26 20:55:38 +0900

    add debug output

--- src/ros_trace_points.cpp
+++ src/ros_trace_points.cpp
@@ -22,6 +22,8 @@
 #include "caret_trace/tracing_controller.hpp"
 #include "caret_trace/singleton.hpp"
 
+// #define DEBUG_OUTPUT
+
 extern "C" {
 void ros_trace_rcl_node_init(
   const void * node_handle,
@@ -43,6 +45,14 @@ void ros_trace_rcl_node_init(
 
   using functionT = void (*)(const void *, const void *, const char *, const char *);
   ((functionT) orig_func)(node_handle, rmw_handle, node_name, node_namespace);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rcl_node_init," <<
+    node_handle << "," <<
+    rmw_handle << "," <<
+    node_name << "," <<
+    node_namespace << std::endl;
+#endif
 }
 
 void ros_trace_rcl_subscription_init(
@@ -62,6 +72,15 @@ void ros_trace_rcl_subscription_init(
   ((functionT) orig_func)(
     subscription_handle, node_handle, rmw_subscription_handle, topic_name,
     queue_depth);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rcl_subscription_init," <<
+    subscription_handle << "," <<
+    node_handle << "," <<
+    rmw_subscription_handle << "," <<
+    topic_name << "," <<
+    queue_depth << std::endl;
+#endif
 }
 
 void ros_trace_rclcpp_subscription_init(
@@ -75,6 +94,12 @@ void ros_trace_rclcpp_subscription_init(
 
   using functionT = void (*)(const void *, const void *);
   ((functionT) orig_func)(subscription_handle, subscription);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rclcpp_subscription_init," <<
+    subscription_handle << "," <<
+    subscription << std::endl;
+#endif
 }
 
 void ros_trace_rclcpp_subscription_callback_added(
@@ -88,6 +113,12 @@ void ros_trace_rclcpp_subscription_callback_added(
 
   using functionT = void (*)(const void *, const void *);
   ((functionT) orig_func)(subscription, callback);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rclcpp_subscription_callback_added," <<
+    subscription << "," <<
+    callback << std::endl;
+#endif
 }
 
 void ros_trace_rclcpp_timer_callback_added(const void * timer_handle, const void * callback)
@@ -99,6 +130,12 @@ void ros_trace_rclcpp_timer_callback_added(const void * timer_handle, const void
 
   using functionT = void (*)(const void *, const void *);
   ((functionT) orig_func)(timer_handle, callback);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rclcpp_timer_callback_added," <<
+    timer_handle << "," <<
+    callback << std::endl;
+#endif
 }
 
 void ros_trace_rclcpp_timer_link_node(const void * timer_handle, const void * node_handle)
@@ -110,6 +147,12 @@ void ros_trace_rclcpp_timer_link_node(const void * timer_handle, const void * no
 
   using functionT = void (*)(const void *, const void *);
   ((functionT) orig_func)(timer_handle, node_handle);
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "rclcpp_timer_link_node," <<
+    timer_handle << "," <<
+    node_handle << std::endl;
+#endif
 }
 
 void ros_trace_callback_start(const void * callback, bool is_intra_process)
@@ -122,6 +165,12 @@ void ros_trace_callback_start(const void * callback, bool is_intra_process)
   if (controller.is_allowed_callback(callback)) {
     ((functionT) orig_func)(callback, is_intra_process);
   }
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "callback_start," <<
+    callback << "," <<
+    is_intra_process << std::endl;
+#endif
 }
 
 void ros_trace_callback_end(const void * callback)
@@ -133,6 +182,11 @@ void ros_trace_callback_end(const void * callback)
   if (controller.is_allowed_callback(callback)) {
     ((functionT) orig_func)(callback);
   }
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "callback_end," <<
+    callback << std::endl;
+#endif
 }
 
 void ros_trace_dispatch_subscription_callback(
@@ -147,6 +201,13 @@ void ros_trace_dispatch_subscription_callback(
   if (controller.is_allowed_callback(callback)) {
     ((functionT) orig_func)(message, callback, source_timestamp);
   }
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "dispatch_subscription_callback," <<
+    message << "," <<
+    callback << "," <<
+    source_timestamp << std::endl;
+#endif
 }
 
 void ros_trace_dispatch_intra_process_subscription_callback(
@@ -160,5 +221,228 @@ void ros_trace_dispatch_intra_process_subscription_callback(
   if (controller.is_allowed_callback(callback)) {
     ((functionT) orig_func)(message, callback);
   }
+
+#ifdef DEBUG_OUTPUT
+  std::cerr << "dispatch_intra_process_subscription_callback," <<
+    message << "," <<
+    callback << std::endl;
+#endif
 }
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rclcpp_publish(
+  const void * publisher_handle,
+  const void * message)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(publisher_handle, message);
+
+  std::cerr << "rclcpp_publish," <<
+    publisher_handle << "," <<
+    message << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rclcpp_intra_publish(
+  const void * publisher_handle,
+  const void * message)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(publisher_handle, message);
+
+  std::cerr << "rclcpp_intra_publish," <<
+    publisher_handle << "," <<
+    message << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_timer_init(
+  const void * timer_handle,
+  int64_t period)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, int64_t);
+  ((functionT) orig_func)(timer_handle, period);
+
+  std::cerr << "rcl_timer_init," <<
+    timer_handle << "," <<
+    period << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_init(
+  const void * context_handle)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *);
+  ((functionT) orig_func)(context_handle);
+
+  std::cerr << "rcl_init," <<
+    context_handle << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_publisher_init(
+  const void * publisher_handle,
+  const void * node_handle,
+  const void * rmw_publisher_handle,
+  const char * topic_name,
+  const size_t queue_depth
+)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *, const void *, const char *, const size_t);
+  ((functionT) orig_func)(
+    publisher_handle,
+    node_handle,
+    rmw_publisher_handle,
+    topic_name,
+    queue_depth);
+
+  std::cerr << "rcl_publisher_init," <<
+    publisher_handle << "," <<
+    node_handle << "," <<
+    rmw_publisher_handle << "," <<
+    topic_name << "," <<
+    queue_depth << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_publish(
+  const void * publisher_handle,
+  const void * message)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(publisher_handle, message);
+
+  std::cerr << "rcl_publish," <<
+    publisher_handle << "," <<
+    message << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_service_init(
+  const void * service_handle,
+  const void * node_handle,
+  const void * rmw_service_handle,
+  const char * service_name)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *, const void *, const char *);
+  ((functionT) orig_func)(service_handle, node_handle, rmw_service_handle, service_name);
+
+  std::cerr << "rcl_service_init," <<
+    service_handle << "," <<
+    node_handle << "," <<
+    rmw_service_handle << "," <<
+    service_name << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rclcpp_service_callback_added(
+  const void * service_handle,
+  const char * callback)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(service_handle, callback);
+
+  std::cerr << "rclcpp_service_callback_added," <<
+    service_handle << "," <<
+    callback << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_client_init(
+  const void * client_handle,
+  const void * node_handle,
+  const void * rmw_client_handle,
+  const char * service_name)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *, const void *, const char *);
+  ((functionT) orig_func)(client_handle, node_handle, rmw_client_handle, service_name);
+
+  std::cerr << "rcl_client_init," <<
+    client_handle << "," <<
+    node_handle << "," <<
+    rmw_client_handle << "," <<
+    service_name << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rclcpp_callback_register(
+  const void * callback,
+  const char * symbol)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const char *);
+  ((functionT) orig_func)(callback, symbol);
+
+  std::cerr << "rclcpp_callback_register," <<
+    callback << "," <<
+    symbol << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_lifecycle_state_machine_init(
+  const void * node_handle,
+  const void * state_machine)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(node_handle, state_machine);
+
+  std::cerr << "rcl_lifecycle_state_machine_init," <<
+    node_handle << "," <<
+    state_machine << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_rcl_lifecycle_transition(
+  const void * state_machine,
+  const char * start_label,
+  const char * goal_label)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const char *, const char *);
+  ((functionT) orig_func)(state_machine, start_label, goal_label);
+
+  std::cerr << "rcl_lifecycle_transition," <<
+    state_machine << "," <<
+    start_label << "," <<
+    goal_label << "," << std::endl;
+}
+#endif
+
+#ifdef DEBUG_OUTPUT
+void ros_trace_message_construct(
+  const void * original_message,
+  const void * constructed_message)
+{
+  static void * orig_func = dlsym(RTLD_NEXT, __func__);
+  using functionT = void (*)(const void *, const void *);
+  ((functionT) orig_func)(original_message, constructed_message);
+
+  std::cerr << "message_construct," <<
+    original_message << "," <<
+    constructed_message << std::endl;
+}
+#endif
 }
