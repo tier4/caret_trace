e628c3082835f643a41046a2309258d7eb850fcc
hsgwa <19860128+hsgwa@users.noreply.github.com>
2022-10-05 19:21:44 +0900

    feat: add APIs to HashableKeys (#52)
    * add: comment for hash function
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * remove redundant comment
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * fixe a bug that equals uses only the first argument
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * extend to 5 arguments
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * add :get api(first, second, ... fifth)
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * add less operator
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * add: size tests
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * add: modify string literal case
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * pre-commit
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * typo
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * fix error in release build
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>

--- CARET_trace/include/caret_trace/keys_set.hpp
+++ CARET_trace/include/caret_trace/keys_set.hpp
@@ -16,12 +16,47 @@
 
 #include <functional>
 #include <iostream>
+#include <string>
 #include <type_traits>
 #include <unordered_set>
 
-template <typename T1, typename T2 = std::false_type, typename T3 = std::false_type>
+template <bool Cond, typename Then, typename Else>
+struct if_
+{
+  using type = Then;
+};
+
+template <typename Then, typename Else>
+struct if_<false, Then, Else>
+{
+  using type = Else;
+};
+
+// clang-format off
+template<
+  typename T1,
+  typename T2 = std::false_type,
+  typename T3 = std::false_type,
+  typename T4 = std::false_type,
+  typename T5 = std::false_type
+>
+// clang-format on
 class HashableKeys
 {
+private:
+  using IsT1String = std::is_same<const char *, T1>;
+  using IsT2String = std::is_same<const char *, T2>;
+  using IsT3String = std::is_same<const char *, T3>;
+  using IsT4String = std::is_same<const char *, T4>;
+  using IsT5String = std::is_same<const char *, T5>;
+
+  // Store string literal as std::string
+  using T1_ = typename if_<IsT1String::value, std::string, T1>::type;
+  using T2_ = typename if_<IsT2String::value, std::string, T2>::type;
+  using T3_ = typename if_<IsT3String::value, std::string, T3>::type;
+  using T4_ = typename if_<IsT4String::value, std::string, T4>::type;
+  using T5_ = typename if_<IsT5String::value, std::string, T5>::type;
+
 public:
   explicit HashableKeys(T1 key1) : key1_(key1) {}
 
@@ -29,56 +64,164 @@ public:
 
   HashableKeys(T1 key1, T2 key2, T3 key3) : key1_(key1), key2_(key2), key3_(key3) {}
 
+  HashableKeys(T1 key1, T2 key2, T3 key3, T4 key4)
+  : key1_(key1), key2_(key2), key3_(key3), key4_(key4)
+  {
+  }
+
+  HashableKeys(T1 key1, T2 key2, T3 key3, T4 key4, T5 key5)
+  : key1_(key1), key2_(key2), key3_(key3), key4_(key4), key5_(key5)
+  {
+  }
+
   size_t hash() const
   {
+    // For hash code implementations, see:
+    // https://www.baeldung.com/java-hashcode#standard-hashcode-implementations
+
     size_t res = 17;
     if constexpr (std::true_type::value) {
-      res = res * 31 + std::hash<T1>()(key1_);
+      res = res * 31 + std::hash<T1_>()(key1_);
     }
     if constexpr (!std::is_same<std::false_type, T2>::value) {
-      res = res * 31 + std::hash<T2>()(key2_);
+      res = res * 31 + std::hash<T2_>()(key2_);
     }
     if constexpr (!std::is_same<std::false_type, T3>::value) {
-      res = res * 31 + std::hash<T3>()(key3_);
+      res = res * 31 + std::hash<T3_>()(key3_);
+    }
+    if constexpr (!std::is_same_v<std::false_type, T4>) {
+      res = res * 31 + std::hash<T4_>()(key4_);
+    }
+    if constexpr (!std::is_same_v<std::false_type, T5>) {
+      res = res * 31 + std::hash<T5_>()(key5_);
     }
 
     return res;
   }
-  bool equal_to(const HashableKeys<T1, T2, T3> & keys) const
+  bool equal_to(const HashableKeys<T1, T2, T3, T4, T5> & keys) const
   {
-    if constexpr (std::true_type::value) {
-      return key1_ == keys.key1_;
-    } else if constexpr (!std::is_same<std::false_type, T2>::value) {
+    if constexpr (!std::is_same_v<std::false_type, T5>) {
+      return key1_ == keys.key1_ && key2_ == keys.key2_ && key3_ == keys.key3_ &&
+             key4_ == keys.key4_ && key5_ == keys.key5_;
+    } else if constexpr (!std::is_same_v<std::false_type, T4>) {
+      return key1_ == keys.key1_ && key2_ == keys.key2_ && key3_ == keys.key3_ &&
+             key4_ == keys.key4_;
+    } else if constexpr (!std::is_same_v<std::false_type, T3>) {
+      return key1_ == keys.key1_ && key2_ == keys.key2_ && key3_ == keys.key3_;
+    } else if constexpr (!std::is_same_v<std::false_type, T2>) {
       return key1_ == keys.key1_ && key2_ == keys.key2_;
-    } else if constexpr (!std::is_same<std::false_type, T3>::value) {
-      return key1_ == keys.key1_ && key2_ == keys.key2_ && key3_ == keys.keys3_;
+    } else {
+      return key1_ == keys.key1_;
+    }
+  }
+
+  bool operator<(const HashableKeys<T1, T2, T3, T4, T5> & rhs) const
+  {
+    if (first() < rhs.first()) {
+      return true;
+    }
+
+    if constexpr (!std::is_same_v<std::false_type, T2>) {
+      if (second() < rhs.second()) {
+        return true;
+      }
+    }
+
+    if constexpr (!std::is_same_v<std::false_type, T3>) {
+      if (third() < rhs.third()) {
+        return true;
+      }
+    }
+
+    if constexpr (!std::is_same_v<std::false_type, T4>) {
+      if (fourth() < rhs.fourth()) {
+        return true;
+      }
+    }
+
+    if constexpr (!std::is_same_v<std::false_type, T5>) {
+      if (fifth() < rhs.fifth()) {
+        return true;
+      }
     }
 
     return false;
   }
 
+  T1 first() const
+  {
+    if constexpr (IsT1String::value) {
+      return key1_.c_str();
+    } else {
+      return key1_;
+    }
+  }
+
+  T2 second() const
+  {
+    static_assert(!std::is_same_v<T2, std::false_type>, "Invalid access.");
+
+    if constexpr (IsT2String::value) {
+      return key2_.c_str();
+    } else {
+      return key2_;
+    }
+  }
+
+  T3 third() const
+  {
+    static_assert(!std::is_same_v<T3, std::false_type>, "Invalid access.");
+
+    if constexpr (IsT3String::value) {
+      return key3_.c_str();
+    } else {
+      return key3_;
+    }
+  }
+
+  T4 fourth() const
+  {
+    static_assert(!std::is_same_v<T4, std::false_type>, "Invalid access.");
+
+    if constexpr (IsT4String::value) {
+      return key4_.c_str();
+    } else {
+      return key4_;
+    }
+  }
+
+  T5 fifth() const
+  {
+    static_assert(!std::is_same_v<T5, std::false_type>, "Invalid access.");
+
+    if constexpr (IsT5String::value) {
+      return key5_.c_str();
+    } else {
+      return key5_;
+    }
+  }
+
 private:
-  T1 key1_;
-  T2 key2_;
-  T3 key3_;
+  T1_ key1_;
+  T2_ key2_;
+  T3_ key3_;
+  T4_ key4_;
+  T5_ key5_;
 };
 
 namespace std
 {
-template <typename T1, typename T2, typename T3>
-struct hash<HashableKeys<T1, T2, T3>>
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+struct hash<HashableKeys<T1, T2, T3, T4, T5>>
 {
-  size_t operator()(const HashableKeys<T1, T2, T3> & t) const
-  {
-    // cout << t.hash() << endl;
-    return t.hash();
-  }
+  size_t operator()(const HashableKeys<T1, T2, T3, T4, T5> & t) const { return t.hash(); }
 };
 
-template <typename T1, typename T2, typename T3>
-struct equal_to<HashableKeys<T1, T2, T3>>
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+struct equal_to<HashableKeys<T1, T2, T3, T4, T5>>
 {
-  size_t operator()(const HashableKeys<T1, T2, T3> & t, const HashableKeys<T1, T2, T3> & t_) const
+  size_t operator()(
+    const HashableKeys<T1, T2, T3, T4, T5> & t, const HashableKeys<T1, T2, T3, T4, T5> & t_) const
   {
     return t.equal_to(t_);
   }
--- CARET_trace/test/test_hashable_keys.cpp
+++ CARET_trace/test/test_hashable_keys.cpp
@@ -35,14 +35,105 @@ TEST(HashableKeys, IntCase)
   }
 
   {
-    HashableKeys<int, int, int> keys(1, 2, 3);
-    HashableKeys<int, int, int> keys_(1, 2, 3);
-    HashableKeys<int, int, int> keys__(2, 2, 3);
+    HashableKeys<int, int, int, int, int> keys(1, 2, 3, 4, 5);
+    HashableKeys<int, int, int, int, int> keys_(1, 2, 3, 4, 5);
+    HashableKeys<int, int, int, int, int> keys__(2, 2, 3, 4, 5);
+    HashableKeys<int, int, int, int, int> keys___(1, 2, 3, 4, 4);
 
     EXPECT_TRUE(keys.equal_to(keys_));
     EXPECT_FALSE(keys.equal_to(keys__));
+    EXPECT_FALSE(keys.equal_to(keys___));
 
     EXPECT_EQ(keys.hash(), keys_.hash());
     EXPECT_NE(keys.hash(), keys__.hash());
+    EXPECT_NE(keys.hash(), keys___.hash());
   }
+}
+
+TEST(HashableKeys, MultiArgsCase)
+{
+  const char * str = "a";
+  const char * str_ = "b";
+
+  HashableKeys<int, char, int64_t, void *, const char *> keys(1, 'a', 2, nullptr, str);
+  HashableKeys<int, char, int64_t, void *, const char *> keys_(1, 'a', 2, nullptr, str);
+  HashableKeys<int, char, int64_t, void *, const char *> keys__(1, 'b', 2, nullptr, str);
+  HashableKeys<int, char, int64_t, void *, const char *> keys___(1, 'a', 2, nullptr, str_);
+
+  EXPECT_EQ(keys.first(), 1);
+  EXPECT_EQ(keys.second(), 'a');
+  EXPECT_EQ(keys.third(), 2);
+  EXPECT_EQ(keys.fourth(), nullptr);
+  EXPECT_NE(keys.fifth(), str);
+  EXPECT_EQ(std::string(keys.fifth()), str);
+
+  EXPECT_TRUE(keys.equal_to(keys_));
+  EXPECT_FALSE(keys.equal_to(keys__));
+  EXPECT_FALSE(keys.equal_to(keys___));
+}
+
+TEST(HashableKeys, LessOperator)
+{
+  HashableKeys<int, int, int, int, int> keys(1, 2, 3, 4, 5);
+  HashableKeys<int, int, int, int, int> keys_(1, 2, 3, 4, 5);
+  HashableKeys<int, int, int, int, int> keys__(1, 2, 3, 4, 6);
+  HashableKeys<int, int, int, int, int> keys___(2, 2, 3, 4, 6);
+
+  EXPECT_FALSE(keys < keys_);
+  EXPECT_TRUE(keys < keys__);
+  EXPECT_TRUE(keys__ < keys___);
+}
+
+TEST(HashableKeys, Size)
+{
+  {
+    HashableKeys<int8_t> keys_8(1);
+    HashableKeys<int16_t> keys_16(1);
+    HashableKeys<int32_t> keys_32(1);
+    HashableKeys<int64_t> keys_64(1);
+
+    EXPECT_LE(sizeof(keys_8), sizeof(keys_16));
+    EXPECT_LE(sizeof(keys_16), sizeof(keys_32));
+    EXPECT_LE(sizeof(keys_32), sizeof(keys_64));
+  }
+
+  {
+    HashableKeys<int> keys_1(1);
+    HashableKeys<int, int> keys_2(1, 2);
+    HashableKeys<int, int, int> keys_3(1, 2, 3);
+    HashableKeys<int, int, int, int> keys_4(1, 2, 3, 4);
+    HashableKeys<int, int, int, int, int64_t> keys_5(1, 2, 3, 4, 5);
+
+    EXPECT_LE(sizeof(keys_1), sizeof(keys_2));
+    EXPECT_LE(sizeof(keys_2), sizeof(keys_3));
+    EXPECT_LE(sizeof(keys_3), sizeof(keys_4));
+    EXPECT_LE(sizeof(keys_4), sizeof(keys_5));
+  }
+}
+
+TEST(HashableKeys, StringLiteralCase)
+{
+  const char * s = "a";
+  auto s_tmp = std::string("a");
+  const char * s_ = s_tmp.c_str();  // avoid optimization of string literal
+  const char * s__ = "b";
+
+  EXPECT_NE(s, s_);
+
+  HashableKeys<const char *> keys(s);
+  HashableKeys<const char *> keys_(s_);
+  HashableKeys<const char *> keys__(s__);
+
+  auto is_equal = [](const char * lhs, const char * rhs) -> bool {
+    return std::string(lhs).compare(rhs) == 0;
+  };
+
+  EXPECT_TRUE(is_equal(keys.first(), keys_.first()));
+  EXPECT_FALSE(is_equal(keys.first(), keys__.first()));
+
+  EXPECT_TRUE(keys.equal_to(keys_));
+  EXPECT_FALSE(keys.equal_to(keys__));
+
+  EXPECT_EQ(keys.hash(), keys_.hash());
+  EXPECT_NE(keys.hash(), keys__.hash());
 }
