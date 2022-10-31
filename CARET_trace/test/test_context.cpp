3ba01f10e26da5f2f40bc7933d8c750dacc787bc
hsgwa <19860128+hsgwa@users.noreply.github.com>
2022-10-31 19:30:55 +0900

    refactor: add context class (#62)
    * refactor: add context class
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    * fix: build error
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>

---
+++ CARET_trace/test/test_context.cpp
@@ -0,0 +1,31 @@
+// Copyright 2021 Research Institute of Systems Planning, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "caret_trace/context.hpp"
+#include "caret_trace/tracing_controller.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <utility>
+
+TEST(ContextTest, TestGetController)
+{
+  auto controller = std::make_shared<TracingController>();
+  Context context(controller);
+
+  auto & controller_ = context.get_controller();
+  EXPECT_EQ(&controller_, controller.get());
+}
