ed8d50f88e137f5bcd98d6a448e3ed1cf72900ea
ymski <114902604+ymski@users.noreply.github.com>
2024-03-15 19:28:48 +0900

    changed the filter function to always enable for unregistered publishers
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>

--- CARET_trace/src/tracing_controller.cpp
+++ CARET_trace/src/tracing_controller.cpp
@@ -338,6 +338,11 @@ bool TracingController::is_allowed_publisher_handle(const void * publisher_handl
     auto topic_name = publisher_handle_to_topic_names_[publisher_handle];
     auto is_unregistered_publisher_handle = (node_name == "");
 
+    if (is_unregistered_publisher_handle) {
+        allowed_publishers_.insert(std::make_pair(publisher_handle, false));
+        return false;
+      }
+
     if (select_enabled_) {
       auto is_selected_node = partial_match(selected_node_names_, node_name);
       auto is_selected_topic = partial_match(selected_topic_names_, topic_name);
@@ -357,10 +362,6 @@ bool TracingController::is_allowed_publisher_handle(const void * publisher_handl
       auto is_ignored_node = partial_match(ignored_node_names_, node_name);
       auto is_ignored_topic = partial_match(ignored_topic_names_, topic_name);
 
-      if (is_unregistered_publisher_handle) {
-        allowed_publishers_.insert(std::make_pair(publisher_handle, false));
-        return false;
-      }
       if (is_ignored_node && ignored_node_names_.size() > 0) {
         allowed_publishers_.insert(std::make_pair(publisher_handle, false));
         return false;
