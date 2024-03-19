3f1ff54c632f6c881a6900bd9942cf413ace32a2
ymski <yamasaki@isp.co.jp>
2024-03-19 17:30:51 +0900

    feat: filter unregistered pubsiher_handle (#277)
    * feat: filter tracepoints with unregistered pubsiher_handle
    
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>
    
    * ci(pre-commit): autofix
    
    * fix: typo
    
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>
    
    * fix: typo
    
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>
    
    * changed the filter function to always enable for unregistered publishers
    
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>
    
    * ci(pre-commit): autofix
    
    ---------
    
    Signed-off-by: ymski <114902604+ymski@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- CARET_trace/src/tracing_controller.cpp
+++ CARET_trace/src/tracing_controller.cpp
@@ -336,6 +336,12 @@ bool TracingController::is_allowed_publisher_handle(const void * publisher_handl
     auto node_handle = publisher_handle_to_node_handles_[publisher_handle];
     auto node_name = node_handle_to_node_names_[node_handle];
     auto topic_name = publisher_handle_to_topic_names_[publisher_handle];
+    auto is_unregistered_publisher_handle = (node_name == "");
+
+    if (is_unregistered_publisher_handle) {
+      allowed_publishers_.insert(std::make_pair(publisher_handle, false));
+      return false;
+    }
 
     if (select_enabled_) {
       auto is_selected_node = partial_match(selected_node_names_, node_name);
